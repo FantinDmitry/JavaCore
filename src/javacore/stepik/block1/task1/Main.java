961dd8996c7038ae9a9d04a95f05b527ef57c16f
FantinDmitry <fdv.77@mail.ru>
2020-05-14 15:03:56 +0300

    Добавил новую строку122

--- src/javacore/stepik/block1/task1/Main.java
+++ src/javacore/stepik/block1/task1/Main.java
@@ -2,10 +2,17 @@ package javacore.stepik.block1.task1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-/*лоатмидфлгавир*/
+        String a;
+        a = sum(3, 5);
+        System.out.println(a);
+
+    }
+
+    public static String sum(int a, int b) {
+        int c = a + b;
+        if (a + b == 7) {
+            return "Result";
+        }
+        return "Fuck";
     }
 }
