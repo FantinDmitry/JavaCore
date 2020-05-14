c73cb7e71ea8082bc6129e9697db258034abe95d
FantinDmitry <fdv.77@mail.ru>
2020-05-14 14:32:47 +0300

    Добавил новую строку

--- src/javacore/stepik/block1/task1/Main.java
+++ src/javacore/stepik/block1/task1/Main.java
@@ -3,5 +3,6 @@ package javacore.stepik.block1.task1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test");
     }
 }
