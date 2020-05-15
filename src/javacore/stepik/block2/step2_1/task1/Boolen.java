b157db3cd3b8101cfe3fe2ac71420508a2d95d0c
FantinDmitry <fdv.77@mail.ru>
2020-05-15 17:23:22 +0300

    Блок1 закончен+добавленна структура добавленна в блок2.Boolen

---
+++ src/javacore/stepik/block2/step2_1/task1/Boolen.java
@@ -0,0 +1,22 @@
+package javacore.stepik.block2.step2_1.task1;
+
+public class Boolen {
+    public static void main(String[] args) {
+
+        System.out.println(1 == 1);
+        System.out.println(2 == 3);
+        System.out.println("^ and !=");
+        System.out.println(1 == 1 ^ 2 == 3);
+        System.out.println((1 == 1) != (2 == 3));
+        System.out.println("| and ||");
+        System.out.println((1 == 1) | (2 == 3));
+        System.out.println((1 == 1) || (2 == 3));
+        System.out.println("& and ==");
+        System.out.println((1 == 1) & (2 == 3));
+        System.out.println((1 == 1) == (2 == 3));
+        System.out.println("| and !=");
+        System.out.println((1 == 1) | (2 == 3));
+        System.out.println((1 == 1) != (2 == 3));
+
+    }
+}
