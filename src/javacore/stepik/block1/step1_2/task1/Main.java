2ea254612c938108b667e4465edb22de0e21c6c0
FantinDmitry <fdv.77@mail.ru>
2020-05-19 21:09:16 +0300

    task 2 (false;false;false;)

---
+++ src/javacore/stepik/block1/step1_2/task1/Main.java
@@ -0,0 +1,9 @@
+package javacore.stepik.block1.step1_2.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("It's alive! It's alive!");
+
+    }
+    }
+
---
+++ src/javacore/stepik/block2/step2_1/task2/Boolen_1.java
@@ -0,0 +1,17 @@
+package javacore.stepik.block2.step2_1.task2;
+
+public class Boolen_1 {
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = false;
+        boolean c = false;
+        boolean d= false;
+
+        System.out.println( a | b);
+        System.out.println( b & c);
+        System.out.println( c ^ d);
+
+
+    }
+
+}
