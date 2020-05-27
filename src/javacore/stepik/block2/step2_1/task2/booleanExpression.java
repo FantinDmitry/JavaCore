9176931d90a02e8ccf2ff4ffc3031c478efc0dec
FantinDmitry <65277039+FantinDmitry@users.noreply.github.com>
2020-05-27 23:57:41 +0300

    Update booleanExpression.java

--- src/javacore/stepik/block2/step2_1/task2/booleanExpression.java
+++ src/javacore/stepik/block2/step2_1/task2/booleanExpression.java
@@ -3,7 +3,7 @@ package javacore.stepik.block2.step2_1.task2;
 public class booleanExpression {
     public static void main(String[] args) {
 
-
+// Дописать код из Stepik (false;false;....)
 
 
     }
